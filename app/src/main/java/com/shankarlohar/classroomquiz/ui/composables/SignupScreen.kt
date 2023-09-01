595651d99ce1f05f9df935bd5f39ce36706a8bbe
shankarlohar <shankarloharmail@gmail.com>
2023-09-02 01:29:34 +0530

    signup screen ui completed

--- app/src/main/java/com/shankarlohar/classroomquiz/ui/composables/SignupScreen.kt
+++ app/src/main/java/com/shankarlohar/classroomquiz/ui/composables/SignupScreen.kt
@@ -1,9 +1,14 @@
 package com.shankarlohar.classroomquiz.ui.composables
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.navigation.NavHostController
 import androidx.compose.material3.Text
 import androidx.compose.material3.Button
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
@@ -13,10 +18,12 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.unit.dp
+import com.shankarlohar.classroomquiz.ui.custom.CustomStyledTextField
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SignupScreen(navController: NavHostController) {
     var email by remember { mutableStateOf("") }
@@ -27,86 +34,93 @@ fun SignupScreen(navController: NavHostController) {
     var school by remember { mutableStateOf("") }
     var department by remember { mutableStateOf("") }
 
-    Column(
-        modifier = Modifier.fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
-    ) {
-        Text(text = "Signup as Teacher")
+    val scrollState = rememberScrollState()
 
-        TextField(
-            value = email,
-            onValueChange = { email = it },
-            label = { Text("Email") },
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .border(
+                width = 1.dp,
+                color = Color.White,
+                shape = RoundedCornerShape(8.dp) // Optional, if you want rounded corners
+            )
+    ){
+        Column(
             modifier = Modifier
-                .fillMaxWidth()
-                .padding(16.dp)
-        )
+                .fillMaxSize()
+                .verticalScroll(scrollState)
+                .padding(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Text(
+                text = "Hey! Let's get you started.",
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier.padding(16.dp)
+            )
 
-        TextField(
-            value = password,
-            onValueChange = { password = it },
-            label = { Text("Password") },
-            visualTransformation = PasswordVisualTransformation(),
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(16.dp)
-        )
+            CustomStyledTextField(
+                value = email,
+                onValueChange = { email = it },
+                label = "Your Email",
 
-        TextField(
-            value = confirmPassword,
-            onValueChange = { confirmPassword = it },
-            label = { Text("Confirm Password") },
-            visualTransformation = PasswordVisualTransformation(),
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(16.dp)
-        )
+                )
 
-        TextField(
-            value = firstName,
-            onValueChange = { firstName = it },
-            label = { Text("First Name") },
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(16.dp)
-        )
+            CustomStyledTextField(
+                value = password,
+                onValueChange = { password = it },
+                label = "Password",
+                visualTransformation = PasswordVisualTransformation(),
 
-        TextField(
-            value = lastName,
-            onValueChange = { lastName = it },
-            label = { Text("Last Name") },
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(16.dp)
-        )
+                )
 
-        TextField(
-            value = school,
-            onValueChange = { school = it },
-            label = { Text("School") },
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(16.dp)
-        )
+            CustomStyledTextField(
+                value = confirmPassword,
+                onValueChange = { confirmPassword = it },
+                label = "Confirm Password",
+                visualTransformation = PasswordVisualTransformation(),
 
-        TextField(
-            value = department,
-            onValueChange = { department = it },
-            label = { Text("Department") },
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(16.dp)
-        )
+                )
 
-        Button(onClick = {
-            // Validate the entered details
-            // Create an instance of the Teacher data class
-            // Navigate to home screen
-            navController.navigate("homeScreen")
-        }) {
-            Text("Signup")
+            CustomStyledTextField(
+                value = firstName,
+                onValueChange = { firstName = it },
+                label = "First Name",
+
+                )
+
+            CustomStyledTextField(
+                value = lastName,
+                onValueChange = { lastName = it },
+                label = "Last Name",
+
+                )
+
+            CustomStyledTextField(
+                value = school,
+                onValueChange = { school = it },
+                label = "School",
+
+                )
+
+            CustomStyledTextField(
+                value = department,
+                onValueChange = { department = it },
+                label = "Department",
+
+                )
+
+            Button(onClick = {
+                // Validate the entered details
+                // Create an instance of the Teacher data class
+                // Navigate to home screen
+                navController.navigate("homeScreen")
+            }) {
+                Text("Signup")
+            }
         }
     }
+
+
 }
 
