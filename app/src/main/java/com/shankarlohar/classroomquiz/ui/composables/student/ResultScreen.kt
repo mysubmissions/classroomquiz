a5e3be99a73d7197e9e3406e9eff5633274ad1ad
shankarlohar <shankarloharmail@gmail.com>
2023-09-02 02:48:01 +0530

    result screen student

--- app/src/main/java/com/shankarlohar/classroomquiz/ui/composables/student/ResultScreen.kt
+++ app/src/main/java/com/shankarlohar/classroomquiz/ui/composables/student/ResultScreen.kt
@@ -1,2 +1,36 @@
-package com.shankarlohar.classroomquiz.ui.composables
+package com.shankarlohar.classroomquiz.ui.composables.student
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+
+@Composable
+fun ResultScreen(navController: NavController, correctAnswers: Int, questionsSize: Int) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(
+            text = "Quiz Completed!",
+            color = Color.Green,
+            modifier = Modifier.padding(16.dp)
+        )
+        Text(
+            text = "Your Score: $correctAnswers/$questionsSize",
+            modifier = Modifier.padding(16.dp)
+        )
+        Button(onClick = { navController.navigate("loginScreen") }) {
+            Text("Exit Quiz")
+        }
+    }
+}
