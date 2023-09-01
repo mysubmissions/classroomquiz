8e0170ec4b6e52db695d67cd94410ce741bfc88e
shankarlohar <shankarloharmail@gmail.com>
2023-09-02 02:48:38 +0530

    leaderboardscreen

--- app/src/main/java/com/shankarlohar/classroomquiz/ui/composables/teacher/LeaderboardScreen.kt
+++ app/src/main/java/com/shankarlohar/classroomquiz/ui/composables/teacher/LeaderboardScreen.kt
@@ -1,10 +1,9 @@
-package com.shankarlohar.classroomquiz.ui.composables
+package com.shankarlohar.classroomquiz.ui.composables.teacher
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
